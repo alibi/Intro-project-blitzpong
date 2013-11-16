a855576ec96ad4a549634139adcac8d93d27e929
alibi <ali.bischak@gmail.com>
2013-11-16 02:31:20 -0500

    Create leaderboards.php

---
+++ leaderboards.php
@@ -0,0 +1,22 @@
+<?php include 'header.php'; ?>
+    <table align="center" border="1" class="leaderboard" >
+      <tr>
+        <th> Player Name </th>
+        <th> Wins-Losses </th>
+        <th> Games Played </th>
+        <th> Win % </th>
+      </tr>
+      <tr>
+        <td> A </td>
+        <td> 4-0 </td>
+        <td> 4 </td>
+        <td> 100% </td>
+      </tr>
+      <tr>
+        <td> B </td>
+        <td> 0-4 </td>
+        <td> 4 </td>
+        <td> 0% </td>
+      </tr>
+    </table>
+<?php include 'footer.php'; ?>
