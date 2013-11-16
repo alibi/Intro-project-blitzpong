ebc8841e92a986c0925006e73956847a213b5ffa
alibi <ali.bischak@gmail.com>
2013-11-16 02:32:49 -0500

    Create playerpager.php

---
+++ playerpager.php
@@ -0,0 +1,27 @@
+<?php include 'header.php'; ?>
+  <h2>Player Name</h2>
+  <h3>High Scores</h3>
+    <table align="center" border="1" class="highscores" >
+      <tr>
+        <th> Score </th>
+        <th> Wins-Losses </th>
+        <th> Date </th>
+        <th> Win % </th>
+        <th> Versus </th>
+      </tr>
+      <tr>
+        <td> 4 </td>
+        <td> 4-0 </td>
+        <td> 11/12/2013 </td>
+        <td> 100% </td>
+        <td> Comp </td>
+      </tr>
+      <tr>
+        <td> 8 </td>
+        <td> 8-6 </td>
+        <td> 8/16/2012 </td>
+        <td> 75% </td>
+        <td> P2 </td>
+      </tr>
+    </table>
+<?php include 'footer.php'; ?>
